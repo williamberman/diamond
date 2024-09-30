f0bfeb6f2390a9b5db4a3624a0438b9be98936f6
Will Berman <wlbberman@gmail.com>
2024-09-30 17:19:22 +0000

    more

--- src/searchy_boi.py
+++ src/searchy_boi.py
@@ -49,8 +49,8 @@ def main(cfg_: DictConfig):
 
                 scatter_data = [obs_send.shape, actions_send.shape, True, env_loop_.ctr]
 
-                obs_send = [obs_send] * dist.get_world_size()
-                actions_send = [actions_send] * dist.get_world_size()
+                obs_send = [obs_send for _ in range(dist.get_world_size())]
+                actions_send = [actions_send for _ in range(dist.get_world_size())]
             else:
                 scatter_data = [None, None, False, env_loop_.ctr]
                 obs_send, actions_send = None, None
@@ -58,7 +58,7 @@ def main(cfg_: DictConfig):
             scatter_data = None
             obs_send, actions_send = None, None
 
-        scatter_data = [scatter_data] * dist.get_world_size()
+        scatter_data = [scatter_data for _ in range(dist.get_world_size())]
         scatter_data_output = [None]
         dist.scatter_object_list(scatter_data_output, scatter_data, src=0)
         obs_shape, actions_shape, do_scatter, ctr = scatter_data_output[0]
@@ -71,11 +71,12 @@ def main(cfg_: DictConfig):
             dist.scatter(actions, actions_send, 0)
 
             action, best_rew = choose_action(obs, actions, ctr)
+            best_rew = best_rew.item()
 
             # gather best_rew
             best_rew = torch.tensor([best_rew], device=device)
             if device == 0:
-                best_rew_target = [best_rew] * dist.get_world_size()
+                best_rew_target = [torch.zeros_like(best_rew) for _ in range(dist.get_world_size())]
             else:
                 best_rew_target = None
             dist.gather(best_rew, best_rew_target, 0)
@@ -83,7 +84,7 @@ def main(cfg_: DictConfig):
             # gather action
             action = torch.tensor([action], device=device)
             if device == 0:
-                action_target = [action] * dist.get_world_size()
+                action_target = [torch.zeros_like(action) for _ in range(dist.get_world_size())]
             else:
                 action_target = None
             dist.gather(action, action_target, 0)
@@ -93,12 +94,13 @@ def main(cfg_: DictConfig):
                 action = torch.concat(action_target, dim=0).tolist()
                 action_rew = zip(action, best_rew)
                 action_rew = sorted(action_rew, key=lambda x: x[1], reverse=True)
+                print(f"device {device}: chose best action {action_rew[0][0]} with rew {action_rew[0][1]}")
                 action = action_rew[0][0]
 
         if device == 0:
             end = env_loop_.step2(action)
             end = torch.tensor([end], device=device)
-            scatter_data = [end] * dist.get_world_size()
+            scatter_data = [end for _ in range(dist.get_world_size())]
         else:
             end = torch.tensor([False], device=device)
             scatter_data = None
@@ -119,6 +121,7 @@ def init_global_state():
 
     # ckpt = "/mnt/raid/diamond/better4/Breakout_100k_labeled_1000_actor_critic_cont/checkpoints/agent_versions/agent_epoch_01000.pt"
     ckpt = "/workspace/Breakout.pt"
+    # ckpt = "/workspace/my_wm.pt"
     agent = Agent(instantiate(cfg.agent, num_actions=num_actions)).to(device, dtype=torch.bfloat16)
     agent.load_state_dict(torch.load(ckpt, map_location=torch.device("cpu")))
     agent.eval()
@@ -179,17 +182,25 @@ class env_loop:
 
         return end or trunc
 
+# save_prefix = "/workspace/run_see_if_access_to_fire_helps/"
+save_prefix = "/workspace/running_with_theirs_2/"
+if device == 0:
+    os.makedirs(save_prefix, exist_ok=True)
+
 def save(obs, idx):
     if isinstance(obs, list):
         obs = torch.cat(obs, dim=0)
     obs = obs.permute(0, 2, 3, 1).mul(0.5).add(0.5).mul(255).clamp(0, 255).byte().cpu().numpy()
     obs = [Image.fromarray(x).resize((256, 256)) for x in obs]
-    imageio.mimsave(f"/workspace/obs_{idx}.mp4", obs, fps=5)
+    imageio.mimsave(os.path.join(save_prefix, f"obs_{idx}.mp4"), obs, fps=1)
+    os.makedirs(os.path.join(save_prefix, f"obs_{idx}"), exist_ok=True)
+    for i, x in enumerate(obs):
+        x.save(os.path.join(save_prefix, f"obs_{idx}/{i}.png"))
     if device == 0:
         print("saved")
 
 # available_sample_actions = [0, 2, 3]
-available_sample_actions = [2, 3]
+available_sample_actions = [0, 1, 2, 3]
 
 def init_search_actions(n):
     acts = [[x] for x in available_sample_actions]
@@ -215,14 +226,20 @@ def choose_action(obs, actions, ctr):
     if obs.shape[1] < num_steps_conditioning:
         return random.randint(0, num_actions - 1)
 
+    input_seq_len = obs.shape[1]
+
     all_rew = defaultdict(list)
     all_end = defaultdict(list)
     all_obs = defaultdict(list)
     all_actions = defaultdict(list)
 
-    init_search_actions_ = init_search_actions(10)
+    # init_search_actions_ = init_search_actions(10) # when 2 options
+    # init_search_actions_ = init_search_actions(7)
+    # init_search_actions_ = init_search_actions(8) # when 3 options
+    init_search_actions_ = init_search_actions(5)
     init_search_actions_ = [x for i, x in enumerate(init_search_actions_) if i % dist.get_world_size() == device]
-    n_samples = 2
+    n_samples = 1
+    # n_samples = 5
 
     if device == 0:
         print("doing initial rollout")
@@ -231,10 +248,13 @@ def choose_action(obs, actions, ctr):
         print("done initial rollout")
 
     for _ in range(n_samples):
-        obs__, actions__, rew_, end_ = sample_trajectory(obs_, actions_, 15)
+        # obs__, actions__, rew_, end_ = sample_trajectory(obs_, actions_, 15) # when initial search depth 10
+        obs__, actions__, rew_, end_ = sample_trajectory(obs_, actions_, 20)
+        # obs__, actions__, rew_, end_ = sample_trajectory(obs_, actions_, 10)
         rew_ = torch.cat([rew, rew_], dim=1)
         end_ = torch.cat([end, end_], dim=1)
-        rew_ = rew_.sum(dim=-1)
+        # rew_ = rew_.sum(dim=-1)
+        rew_ = rew_.max(dim=-1).values
         end_ = end_.any(dim=-1)
         for init_act, obs___, actions___, rew__, end__ in zip(init_search_actions_, obs__, actions__, rew_, end_):
             all_rew[tuple(init_act)].append(rew__)
@@ -242,7 +262,8 @@ def choose_action(obs, actions, ctr):
             all_obs[tuple(init_act)].append(obs___)
             all_actions[tuple(init_act)].append(actions___)
 
-    avg_rews = {act: sum(rew)/len(rew) for act, rew in all_rew.items()}
+    # avg_rews = {act: sum(rew)/len(rew) for act, rew in all_rew.items()}
+    avg_rews = {act: max(rew) for act, rew in all_rew.items()}
     ends = {act: any(end) for act, end in all_end.items()}
 
     best_rew = 0
@@ -256,15 +277,19 @@ def choose_action(obs, actions, ctr):
             best_rew = avg_rew
             best_action = action
 
+    suffix = f"_best"
     if best_action is None:
         best_action = random.choice(list(avg_rews.keys()))
+        suffix = f"_random"
 
-    if device == 0:
+    # if device == 0:
         # print(f"avg_rews: {avg_rews}, ends: {ends}, best_action: {best_action}, best_rew: {best_rew}")
-        print(f"best_action: {best_action}, best_rew: {best_rew}")
+        # print(f"best_action: {best_action}, best_rew: {best_rew}")
 
-    save(all_obs[best_action][0], f"pred_{ctr}_{device}_0")
-    save(all_obs[best_action][1], f"pred_{ctr}_{device}_1")
+    print(f"device {device}: best_action: {best_action}, best_rew: {best_rew}")
+
+    for i in range(n_samples):
+        save(all_obs[best_action][i][max(input_seq_len-3, 0):], f"pred_{ctr}_{device}_{i}_{best_rew:.4f}_{best_action}{suffix}")
 
     return best_action[0], best_rew
 
@@ -292,7 +317,11 @@ def rollout_trajectory(obs, actions, rollout):
         act_ = act_.contiguous()
         next_obs_ = next_obs_.contiguous()
         rew_, end_, _ = rew_end_model(obs_, act_, next_obs_)
-        rew_ = rew_[:, -1, :].argmax(dim=-1) - 1
+        probs = rew_[:, -1, :].softmax(dim=-1)
+        pos_probs = probs[:, 2]
+        neg_probs = probs[:, 0]
+        # pos_probs = torch.where(pos_probs <= 0.02, torch.tensor([0.0], device=device), pos_probs)
+        rew_ = pos_probs - neg_probs
         end_ = end_[:, -1, :].argmax(dim=-1)
         if rew is None:
             rew = rew_.unsqueeze(1)
@@ -339,7 +368,13 @@ def sample_trajectory(obs, actions, trajectory_length):
         next_obs_ = next_obs_.contiguous()
         rew_, end_, _ = rew_end_model(obs_, act_, next_obs_)
 
-        rew_ = rew_[:, -1, :].argmax(dim=-1, keepdim=True) - 1
+        probs = rew_[:, -1, :].softmax(dim=-1)
+        pos_probs = probs[:, 2]
+        neg_probs = probs[:, 0]
+        # pos_probs = torch.where(pos_probs <= 0.02, torch.tensor([0.0], device=device), pos_probs)
+        rew_ = pos_probs - neg_probs
+        rew_ = rew_.unsqueeze(1)
+
         end_ = end_[:, -1, :].argmax(dim=-1, keepdim=True)
 
         if rew is None:
