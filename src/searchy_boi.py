7bdce7926b12cc697bd52eb10512e243a56f42dd
Will Berman <wlbberman@gmail.com>
2024-10-01 03:48:33 +0000

    more

--- src/searchy_boi.py
+++ src/searchy_boi.py
@@ -14,6 +14,9 @@ from collections import defaultdict
 import torch.distributed as dist
 import os
 import time
+import gc
+import math
+import matplotlib.pyplot as plt
 
 OmegaConf.register_new_resolver("eval", eval)
 
@@ -34,14 +37,18 @@ def main(cfg_: DictConfig):
     dist.init_process_group(backend="nccl")
     init_global_state()
 
+    skip_count = 0
+
     if device == 0:
         env_loop_ = env_loop()
 
     while True:
+        t0 = time.perf_counter()
+
         if device == 0:
             action = env_loop_.step1()
 
-            if action is None:
+            if action is None and skip_count <= 0:
                 obs_send, actions_send = env_loop_.obs, env_loop_.actions
 
                 obs_send = torch.stack(obs_send, dim=1)
@@ -52,6 +59,10 @@ def main(cfg_: DictConfig):
                 obs_send = [obs_send for _ in range(dist.get_world_size())]
                 actions_send = [actions_send for _ in range(dist.get_world_size())]
             else:
+                if skip_count > 0:
+                    action = 0
+                    skip_count -= 1
+
                 scatter_data = [None, None, False, env_loop_.ctr]
                 obs_send, actions_send = None, None
         else:
@@ -70,7 +81,9 @@ def main(cfg_: DictConfig):
             dist.scatter(obs, obs_send, 0)
             dist.scatter(actions, actions_send, 0)
 
-            action = choose_action(obs, actions, ctr)
+            choose_action_out = choose_action(obs, actions, ctr)
+            if device == 0:
+                action, skip_count = choose_action_out
 
         if device == 0:
             end = env_loop_.step2(action)
@@ -81,6 +94,13 @@ def main(cfg_: DictConfig):
             scatter_data = None
 
         dist.scatter(end, scatter_data, 0)
+
+        if do_scatter:
+            gc.collect()
+            torch.cuda.empty_cache()
+
+        if device == 0:
+            print(f"time taken: {time.perf_counter() - t0}")
 
         if end:
             break
@@ -122,6 +142,7 @@ class env_loop:
         self.lives_1_ctr = 0
 
         self.ctr = 0
+        self.first_did_pred = None
 
     def step1(self):
         print(self.ctr)
@@ -141,6 +162,8 @@ class env_loop:
             else:
                 action = None
                 self.did_pred = True
+                if self.first_did_pred is None:
+                    self.first_did_pred = self.ctr
 
         return action
 
@@ -151,7 +174,7 @@ class env_loop:
         self.obs.append(state)
 
         if self.did_pred:
-            save(self.obs, self.ctr)
+            save(self.obs[self.first_did_pred:], self.ctr)
 
         self.ctr += 1
 
@@ -160,7 +183,7 @@ class env_loop:
 # save_prefix = "/workspace/run_see_if_access_to_fire_helps/"
 # save_prefix = "/workspace/running_with_theirs_2/"
 # save_prefix = "/workspace/running_with_theirs_reward_averaging/"
-save_prefix = "/workspace/running_with_theirs_test_refactor/"
+save_prefix = "/workspace/running_with_theirs_test_avg/"
 if device == 0:
     os.makedirs(save_prefix, exist_ok=True)
 
@@ -171,13 +194,15 @@ def save(obs, idx):
     obs = [Image.fromarray(x).resize((256, 256)) for x in obs]
     imageio.mimsave(os.path.join(save_prefix, f"obs_{idx}.mp4"), obs, fps=1)
     os.makedirs(os.path.join(save_prefix, f"obs_{idx}"), exist_ok=True)
-    for i, x in enumerate(obs):
-        x.save(os.path.join(save_prefix, f"obs_{idx}/{i}.png"))
+    # for i, x in enumerate(obs):
+    #     x.save(os.path.join(save_prefix, f"obs_{idx}/{i}.png"))
     if device == 0:
         print("saved")
 
 # available_sample_actions = [0, 2, 3]
-available_sample_actions = [0, 1, 2, 3]
+# available_sample_actions = [0, 1, 2, 3]
+available_sample_actions = [1, 2, 3]
+# available_sample_actions = [2, 3]
 
 def init_search_actions(n):
     acts = [[x] for x in available_sample_actions]
@@ -213,33 +238,120 @@ def choose_action(obs, actions, ctr):
     # init_search_actions_ = init_search_actions(10) # when 2 options
     # init_search_actions_ = init_search_actions(7)
     # init_search_actions_ = init_search_actions(8) # when 3 options
-    init_search_actions_ = init_search_actions(5)
+    # init_search_actions_ = init_search_actions(5)
+    # init_search_actions_ = init_search_actions(15)
+    # init_search_actions_ = init_search_actions(5)
+    init_search_actions_ = init_search_actions(10)
+
     init_search_actions_ = [x for i, x in enumerate(init_search_actions_) if i % dist.get_world_size() == device]
+
     n_samples = 1
     # n_samples = 5
 
-    if device == 0:
-        print("doing initial rollout")
-    obs_, actions_, rew, end = rollout_trajectory(obs, actions, init_search_actions_)
-    if device == 0:
-        print("done initial rollout")
+    mc_traj_len = 20
 
-    for _ in range(n_samples):
-        # obs__, actions__, rew_, end_ = sample_trajectory(obs_, actions_, 15) # when initial search depth 10
-        obs__, actions__, rew_, end_ = sample_trajectory(obs_, actions_, 20)
-        # obs__, actions__, rew_, end_ = sample_trajectory(obs_, actions_, 10)
-        rew_ = torch.cat([rew, rew_], dim=1)
-        end_ = torch.cat([end, end_], dim=1)
-        # rew_ = rew_.sum(dim=-1)
-        rew_ = rew_.max(dim=-1).values
-        end_ = end_.any(dim=-1)
-        for init_act, obs___, actions___, rew__, end__ in zip(init_search_actions_, obs__, actions__, rew_, end_):
-            all_rew[tuple(init_act)].append(rew__)
-            all_end[tuple(init_act)].append(end__)
-            all_obs[tuple(init_act)].append(obs___)
-            all_actions[tuple(init_act)].append(actions___)
+    outter_loop = [x for x in range(0, len(init_search_actions_), 1024)]
 
-    return choose_action_orig(all_obs, all_rew, all_end, input_seq_len, ctr)
+    pbar = tqdm.tqdm(total=len(outter_loop)*(len(init_search_actions_[0])+(n_samples*mc_traj_len)), desc="initial rollout", disable=device != 0)
+
+    for start_idx in outter_loop:
+        end_idx = min(start_idx + 1024, len(init_search_actions_))
+        init_search_actions_batch = init_search_actions_[start_idx:end_idx]
+
+        # if device == 0:
+        #     print("doing initial rollout")
+        obs_, actions_, rew, end = rollout_trajectory(obs, actions, init_search_actions_batch, pbar)
+        # if device == 0:
+        #     print("done initial rollout")
+
+        for _ in range(n_samples):
+            # obs__, actions__, rew_, end_ = sample_trajectory(obs_, actions_, 15) # when initial search depth 10
+            obs__, actions__, rew_, end_ = sample_trajectory(obs_, actions_, mc_traj_len, pbar)
+            # obs__, actions__, rew_, end_ = sample_trajectory(obs_, actions_, 10)
+
+            rew_ = torch.cat([rew, rew_], dim=1)
+            end_ = torch.cat([end, end_], dim=1)
+
+            # rew_ = rew_.sum(dim=-1)
+            rew_ = rew_.max(dim=-1).values
+            end_ = end_.any(dim=-1)
+
+            rew_ = torch.where(end_, torch.tensor([-1.0], device=device), rew_)
+
+            for init_act, obs___, actions___, rew__, end__ in zip(init_search_actions_batch, obs__, actions__, rew_, end_):
+                all_rew[tuple(init_act)].append(rew__)
+                # all_end[tuple(init_act)].append(end__)
+                # all_obs[tuple(init_act)].append(obs___)
+                # all_actions[tuple(init_act)].append(actions___)
+
+        gc.collect()
+        torch.cuda.empty_cache()
+
+    pbar.close()
+
+    # return choose_action_orig(all_obs, all_rew, all_end, input_seq_len, ctr)
+    return choose_action_take_avg(all_rew, ctr)
+
+avg_rews_dbg = {0: [], 1: [], 2: [], 3: []}
+
+def choose_action_take_avg(all_rew, ctr):
+    sum_rews = torch.zeros((max(available_sample_actions)+1,), device=device, dtype=torch.float32)
+    counts = torch.zeros((max(available_sample_actions)+1,), device=device, dtype=torch.float32)
+
+    for idx in range(len(available_sample_actions)):
+        if idx not in available_sample_actions:
+            sum_rews[idx] = float("-inf")
+
+    for actions, rews in all_rew.items():
+        for rew in rews:
+            sum_rews[actions[0]] += rew
+            counts[actions[0]] += 1
+
+    # gather sum_rews and counts to device 0
+    if device == 0:
+        sum_rews_target = [torch.zeros_like(sum_rews) for _ in range(dist.get_world_size())]
+        counts_target = [torch.zeros_like(counts) for _ in range(dist.get_world_size())]
+    else:
+        sum_rews_target = None
+        counts_target = None
+
+    dist.gather(sum_rews, sum_rews_target, 0)
+    dist.gather(counts, counts_target, 0)
+
+    if device == 0:
+        sum_rews = torch.stack(sum_rews_target, dim=1).sum(dim=1)
+        counts = torch.stack(counts_target, dim=1).sum(dim=1)
+        avg_rews = sum_rews / counts
+        action = avg_rews.argmax().item()
+
+        print_str = ", ".join(f"{count} {rew:.4f} {avg_rew:.4f}" for rew, count, avg_rew in zip(sum_rews, counts, avg_rews))
+        print(print_str)
+        print(f"device {device}: chose best action {action} with avg_rew {avg_rews[action].item()}")
+
+        for act in available_sample_actions:
+            it = avg_rews[act].item()
+            if math.isinf(it):
+                it = 0
+            avg_rews_dbg[act].append(it)
+
+        # write out line graph of each avg_rews_dbg[i] to file
+        plt.figure()
+        for act, rew in avg_rews_dbg.items():
+            plt.plot(rew, label=f"action {act}")
+        plt.legend()
+        plt.savefig(os.path.join(save_prefix, f"obs_{ctr}.png"))
+        plt.close()
+
+        skip_count = 10
+        for act in available_sample_actions:
+            if avg_rews[act].item() != 1.0:
+                skip_count = 0
+
+        if skip_count > 0:
+            print(f"All scores are 1.0! skipping for {skip_count} steps")
+
+        return action, skip_count
+
 
 def choose_action_orig(all_obs, all_rew, all_end, input_seq_len, ctr):
     n_samples = len(next(iter(all_obs.values())))
@@ -302,7 +414,7 @@ def choose_action_orig(all_obs, all_rew, all_end, input_seq_len, ctr):
         action = action_rew[0][0]
         return action
 
-def rollout_trajectory(obs, actions, rollout):
+def rollout_trajectory(obs, actions, rollout, pbar):
     assert obs.ndim == 5
     assert actions.ndim == 2
     assert obs.shape[1] == actions.shape[1] + 1
@@ -343,6 +455,8 @@ def rollout_trajectory(obs, actions, rollout):
 
         obs = torch.cat([obs, next_obs.unsqueeze(1)], dim=1)
 
+        pbar.update(1)
+
     return obs, actions, rew, end
 
 def rand_action_tensor(shape):
@@ -351,7 +465,7 @@ def rand_action_tensor(shape):
         n *= x
     return torch.tensor([random.choice(available_sample_actions) for _ in range(n)], dtype=torch.long, device=device).reshape(shape)
 
-def sample_trajectory(obs, actions, trajectory_length):
+def sample_trajectory(obs, actions, trajectory_length, pbar):
     assert obs.ndim == 5
     assert actions.ndim == 2
     assert obs.shape[0] == actions.shape[0]
@@ -360,7 +474,7 @@ def sample_trajectory(obs, actions, trajectory_length):
     rew = None
     end = None
 
-    for _ in tqdm.tqdm(range(trajectory_length), disable=device != 0):
+    for _ in range(trajectory_length):
         actions = torch.cat([actions, rand_action_tensor((actions.shape[0], 1))], dim=1)
 
         obs_ = obs[:, -num_steps_conditioning:]
@@ -396,6 +510,8 @@ def sample_trajectory(obs, actions, trajectory_length):
             end = torch.cat([end, end_], dim=1)
 
         obs = torch.cat([obs, next_obs.unsqueeze(1)], dim=1)
+
+        pbar.update(1)
 
     assert rew.shape[0] == obs.shape[0]
     assert rew.shape[1] == trajectory_length
