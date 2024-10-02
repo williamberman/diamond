8b052e8cc221f8d6b618cb166c384ccaa96a5b20
Will Berman <wlbberman@gmail.com>
2024-10-02 17:06:30 +0000

    remove unused code

--- src/searchy_boi.py
+++ src/searchy_boi.py
@@ -1,5 +1,5 @@
 from agent import Agent
-from envs import make_atari_env, WorldModelEnv
+from envs import make_atari_env
 from hydra.utils import instantiate
 import hydra
 from omegaconf import DictConfig, OmegaConf
@@ -7,9 +7,7 @@ import torch
 import random
 from PIL import Image
 import imageio
-from models.diffusion import Denoiser, DiffusionSampler, DiffusionSamplerConfig
-from models.rew_end_model import RewEndModel
-import tqdm
+from models.diffusion import DiffusionSampler
 from collections import defaultdict
 import torch.distributed as dist
 import os
@@ -17,7 +15,6 @@ import time
 import gc
 import math
 import matplotlib.pyplot as plt
-import functools
 
 OmegaConf.register_new_resolver("eval", eval)
 
@@ -204,35 +201,8 @@ def save(obs, idx):
     if device == 0:
         print("saved")
 
-# available_sample_actions = [0, 2, 3]
 available_sample_actions = [0, 1, 2, 3]
-# available_sample_actions = [1, 2, 3]
-# available_sample_actions = [2, 3]
 
-def init_search_actions(n):
-    acts = [[x] for x in available_sample_actions]
-
-    if n == 1:
-        return acts
-
-    n_1 = init_search_actions(n-1)
-
-    rv = []
-
-    for act in acts:
-        for n_1_act in n_1:
-            rv.append(act + n_1_act)
-
-    return rv
-
-# init_search_actions_ = init_search_actions(10) # when 2 options
-# init_search_actions_ = init_search_actions(7)
-# init_search_actions_ = init_search_actions(8) # when 3 options
-# init_search_actions_ = init_search_actions(5)
-# init_search_actions_ = init_search_actions(15)
-# init_search_actions_ = init_search_actions(5)
-# init_search_actions_ = init_search_actions(10)
-init_search_actions_ = init_search_actions(8)
 
 def choose_action(obs, actions, ctr):
     assert obs.ndim == 5
@@ -321,67 +291,6 @@ def choose_action_take_avg(all_rew, ctr):
 
         return action, skip_count
 
-
-def choose_action_orig(all_obs, all_rew, all_end, input_seq_len, ctr):
-    n_samples = len(next(iter(all_obs.values())))
-
-    # avg_rews = {act: sum(rew)/len(rew) for act, rew in all_rew.items()}
-    avg_rews = {act: max(rew) for act, rew in all_rew.items()}
-    ends = {act: any(end) for act, end in all_end.items()}
-
-    best_rew = 0
-    best_action = None
-
-    for action in avg_rews.keys():
-        avg_rew = avg_rews[action]
-        end = ends[action]
-
-        if avg_rew > best_rew and not end:
-            best_rew = avg_rew
-            best_action = action
-
-    suffix = f"_best"
-    if best_action is None:
-        best_action = random.choice(list(avg_rews.keys()))
-        suffix = f"_random"
-
-    # if device == 0:
-        # print(f"avg_rews: {avg_rews}, ends: {ends}, best_action: {best_action}, best_rew: {best_rew}")
-        # print(f"best_action: {best_action}, best_rew: {best_rew}")
-
-    print(f"device {device}: best_action: {best_action}, best_rew: {best_rew}")
-
-    for i in range(n_samples):
-        save(all_obs[best_action][i][max(input_seq_len-3, 0):], f"pred_{ctr}_{device}_{i}_{best_rew:.4f}_{best_action}{suffix}")
-
-    action = best_action[0]
-
-    best_rew = best_rew.item()
-
-    # gather best_rew
-    best_rew = torch.tensor([best_rew], device=device)
-    if device == 0:
-        best_rew_target = [torch.zeros_like(best_rew) for _ in range(dist.get_world_size())]
-    else:
-        best_rew_target = None
-    dist.gather(best_rew, best_rew_target, 0)
-
-    # gather action
-    action = torch.tensor([action], device=device)
-    if device == 0:
-        action_target = [torch.zeros_like(action) for _ in range(dist.get_world_size())]
-    else:
-        action_target = None
-    dist.gather(action, action_target, 0)
-
-    if device == 0:
-        best_rew = torch.concat(best_rew_target, dim=0).tolist()
-        action = torch.concat(action_target, dim=0).tolist()
-        action_rew = zip(action, best_rew)
-        action_rew = sorted(action_rew, key=lambda x: x[1], reverse=True)
-        print(f"device {device}: chose best action {action_rew[0][0]} with rew {action_rew[0][1]}")
-        action = action_rew[0][0]
-        return action
 
 def rollout_to_depth(obs, actions, depth):
     scattered = False
@@ -563,66 +472,6 @@ def rollout_trajectory_(obs, actions, rollout, pbar, rew, end):
 
         if pbar is not None:
             pbar.update(1)
-
-    return obs, actions, rew, end
-
-def rand_action_tensor(shape):
-    n = 1
-    for x in shape:
-        n *= x
-    return torch.tensor([random.choice(available_sample_actions) for _ in range(n)], dtype=torch.long, device=device).reshape(shape)
-
-def sample_trajectory(obs, actions, trajectory_length, pbar):
-    assert obs.ndim == 5
-    assert actions.ndim == 2
-    assert obs.shape[0] == actions.shape[0]
-    assert obs.shape[1] == actions.shape[1]+1
-
-    rew = None
-    end = None
-
-    for _ in range(trajectory_length):
-        actions = torch.cat([actions, rand_action_tensor((actions.shape[0], 1))], dim=1)
-
-        obs_ = obs[:, -num_steps_conditioning:]
-        act_ = actions[:, -num_steps_conditioning:]
-
-        next_obs = sampler.sample_next_obs(obs_, act_)[0]
-        next_obs_ = torch.cat([obs_[:, 1:], next_obs.unsqueeze(1)], dim=1)
-
-        assert obs_.shape[:2] == next_obs_.shape[:2]
-        assert obs_.shape[:2] == act_.shape[:2]
-
-        obs_ = obs_.contiguous()
-        act_ = act_.contiguous()
-        next_obs_ = next_obs_.contiguous()
-        rew_, end_, _ = rew_end_model(obs_, act_, next_obs_)
-
-        probs = rew_[:, -1, :].softmax(dim=-1)
-        pos_probs = probs[:, 2]
-        neg_probs = probs[:, 0]
-        # pos_probs = torch.where(pos_probs <= 0.02, torch.tensor([0.0], device=device), pos_probs)
-        rew_ = pos_probs - neg_probs
-        rew_ = rew_.unsqueeze(1)
-
-        end_ = end_[:, -1, :].argmax(dim=-1, keepdim=True)
-
-        if rew is None:
-            rew = rew_
-        else:
-            rew = torch.cat([rew, rew_], dim=1)
-        if end is None:
-            end = end_
-        else:
-            end = torch.cat([end, end_], dim=1)
-
-        obs = torch.cat([obs, next_obs.unsqueeze(1)], dim=1)
-
-        pbar.update(1)
-
-    assert rew.shape[0] == obs.shape[0]
-    assert rew.shape[1] == trajectory_length
-    assert end.shape[1] == trajectory_length
 
     return obs, actions, rew, end
 
