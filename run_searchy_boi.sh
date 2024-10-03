43d9d71d48c1d58bc00d532642919a9449b0ebf3
Will Berman <wlbberman@gmail.com>
2024-10-03 21:14:27 +0000

    more

--- run_searchy_boi.sh
+++ run_searchy_boi.sh
@@ -3,7 +3,8 @@
 ctr=0
 
 while true; do
-    export SAVE_PREFIX="/workspace/test_search_$ctr" 
+    export N_STEPS_AT_ONCE=$(($ctr % 5 + 1))
+    export SAVE_PREFIX="/workspace/test_search_${ctr}_${N_STEPS_AT_ONCE}" 
     echo "Saving to $SAVE_PREFIX"
     torchrun --nproc_per_node=8 src/searchy_boi.py
     ctr=$((ctr+1))
